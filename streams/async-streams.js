2fb7eccb22b52c7402d1bdfa73f5abd25fbed090
Marcos Duarte <marcosduarte1994@gmail.com>
2022-11-24 00:03:07 -0300

    async-stream

---
+++ streams/async-streams.js
@@ -0,0 +1,52 @@
+import { pipeline } from 'stream/promises'
+import { setTimeout } from 'timers/promises'
+
+async function * myCustomReadable() {
+  yield Buffer.from('This is my')
+  await setTimeout(1000)
+  yield Buffer.from('custom readable')
+
+}
+
+async function * myCustomTransform(stream) {
+  for await(const chunk of stream) {
+    yield chunk.toString().replace(/\s/g, "_")
+  }
+}
+
+async function * myCustomDuplex(stream) {
+  let bytesRead = 0
+  const wholeString = []
+  for await (const chunk of stream) {
+    console.log(`[duplex writable]`, chunk)
+    bytesRead = chunk.length
+    wholeString.push(chunk)
+  }
+
+  yield `wholeString ${wholeString.join()}`
+  yield `bytesRead ${bytesRead}`
+}
+
+async function * myCustomWritable(stream) {
+  for await(const chunk of stream) {
+    console.log('[Writable]', chunk)
+  }
+}
+
+try {
+  const controller = new AbortController()
+  // Caso precise cancelar um fluxo
+  setImmediate(() => controller.abort())
+  await pipeline(
+    myCustomReadable,
+    myCustomTransform,
+    myCustomDuplex,
+    myCustomWritable,
+    { signal: controller.signal }
+  )
+  
+  console.log(`Process has finished`)
+} catch (error) {
+  console.error('\nAbort', error.message)
+}
+
